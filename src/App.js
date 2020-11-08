f790c76e5e802ff9309f702b83ca4a455c363a9c
Youness BENNAJ <youness.bennaj@gmail.com>
2020-11-08 19:04:33 +0100

    hide API KEY as an environnement variable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL = `https://geo.ipify.org/api/v1?apiKey=${API_KEY}`;
 
 function App() {
 
   useEffect(() => {
     //Here; side-effect code => makes things outside the scope of the function component
+
+    //Typically: an HTTP request to fetch data from an API
+
+    axios.get('')
   });
 
   return (
